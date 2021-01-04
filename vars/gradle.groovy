61328d7379b0ac46abb8824d4ca27f6573d1f747
Maricel Rodriguez <lecirama@gmail.com>
2021-01-04 16:52:14 -0300

    se agregan parametros stage en vars/ejecución

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -3,31 +3,43 @@
 	def ejecucion = load 'script.groovy'
 	ejecucion.call()
 */
+import ejecucion
 def call(){
-    stage('Build&Test'){
-       // env.STG_NAME = 'build & test'
-        sh './gradlew clean build'
-    }
-    stage('Sonar'){
-        //env.STG_NAME = 'sonar'
-        def scannerHome = tool 'sonar';
-        withSonarQubeEnv('sonar'){
-        sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
-    	}
-    }
-    stage('run'){
-        env.STG_NAME = 'run'
-        sh "nohup bash gradlew bootRun &"
-        sleep(10)
-    }
-    stage('Rest'){
-        env.STG_NAME = 'rest'
-        sleep (15)
-        sh 'curl -X GET http://localhost:8086/rest/mscovid/test?msg=testing'
-    }
-    stage('Upload Nexus'){
-        env.STG_NAME = 'UploadNexus'
-        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/maricelrodriguez/.jenkins/workspace/ultib_gradle_feature-dir-inicial/build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
-    }
+    switch (params.Stage){
+        case "Build":
+            stage('Build'){
+            sh './gradlew clean build'
+            }
+        break
+        case "Sonar":
+            stage('Sonar'){
+            //env.STG_NAME = 'sonar'
+            def scannerHome = tool 'sonar';
+            withSonarQubeEnv('sonar'){
+            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+             }
+            }
+        break
+        case "Run":
+            stage('Run'){
+        // env.STG_NAME = 'run'
+            sh "nohup bash gradlew bootRun &"
+            sleep(10)
+            }
+        break
+        case "Rest":
+            stage('Rest'){
+            //env.STG_NAME = 'rest'
+            sleep (15)
+            sh 'curl -X GET http://localhost:8086/rest/mscovid/test?msg=testing'
+            }
+        break
+        case "Nexus":
+            stage('Upload Nexus'){
+            env.STG_NAME = 'UploadNexus'
+            nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/maricelrodriguez/.jenkins/workspace/ultib_gradle_feature-dir-inicial/build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
+            }
+        break
+    }  
  }
 return this;

49815caf84c702bdecad38640a6018f0e105b5de
Maricel Rodriguez <lecirama@gmail.com>
2021-01-03 23:49:09 -0300

    parametro llamado

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -17,7 +17,7 @@ def call(){
 	                   //Invocacion al archivo dependiendo del paramentro generado
 	                    env.STG_NAME=''
 	                    //def build=(params.eleccion == 'Gradle') ? 'gradle.groovy' : 'maven.groovy'
-	                    ejecucion=(params.eleccion == 'Gradle') ? 'gradle.groovy' : 'maven.groovy'
+	                    def ejecucion=(params.eleccion == 'Gradle') ? 'gradle.groovy' : 'maven.groovy'
 	                    //def ejecucion = load build
 	                    ejecucion.call()
 	                 }
